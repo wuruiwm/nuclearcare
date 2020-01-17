872503b0f22de8c17bb9be8a1d4704f95014720f
傍晚升起的太阳 <wuruiwm@qq.com>
2020-01-17 15:32:10 +0800

    生产环境配置

--- config/wx.php
+++ config/wx.php
@@ -5,7 +5,7 @@
  * @Email: wuruiwm@qq.com
  * @Date: 2019-12-27 17:25:31
  * @LastEditors  : 傍晚升起的太阳
- * @LastEditTime : 2020-01-17 14:44:54
+ * @LastEditTime : 2020-01-17 15:32:05
  */
 return [
     'appid'=>'wxb7edf993339d7084',
@@ -13,5 +13,5 @@ return [
     'MCHID'=>'1574276841',
     'KEY'=>'2f546921d89906bf84f28998d006011f',
     'body'=>'合肥由可立家政服务有限公司',
-    'coupon'=>'pages/index/coupon/index',
+    'coupon'=>'pages/me/couponList/index',
 ];
