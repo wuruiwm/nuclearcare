790eb7b9215a13c7639f9d795a8b90fdd4c4e247
傍晚升起的太阳 <wuruiwm@qq.com>
2020-01-17 14:45:13 +0800

    生产环境配置

--- config/wx.php
+++ config/wx.php
@@ -5,13 +5,13 @@
  * @Email: wuruiwm@qq.com
  * @Date: 2019-12-27 17:25:31
  * @LastEditors  : 傍晚升起的太阳
- * @LastEditTime : 2020-01-17 13:58:15
+ * @LastEditTime : 2020-01-17 14:44:54
  */
 return [
     'appid'=>'wxb7edf993339d7084',
     'appsecret'=>'caf22a17f6cdd8af6441f0c4239978ae',
     'MCHID'=>'1574276841',
     'KEY'=>'2f546921d89906bf84f28998d006011f',
-    'body'=>'nuclearcare球鞋洗护',
+    'body'=>'合肥由可立家政服务有限公司',
     'coupon'=>'pages/index/coupon/index',
 ];
